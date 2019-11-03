60e438cb5834add0242988feeb53fd17ccb277a9
Karl Melecio <karlmelecio04@gmail.com>
2019-11-04 01:03:00 +0800

    added comment

--- Kitchen/Kitchen/Minitable.cs
+++ Kitchen/Kitchen/Minitable.cs
@@ -18,6 +18,9 @@ namespace Kitchen
 
         //string myConnectionString = "server=192.168.15.6; uid=root; pwd=; database=menu";
         //string myConnectionString = "server=localhost; uid=root; pwd=; database=menu";
+        
+        //string myConnectionString = "server=192.168.15.6; uid=root; pwd=; database=menu";
+        //string myConnectionString = "server=localhost; uid=root; pwd=; database=menu";
 
         string myConnectionString = "server=192.168.0.105; uid=roota; pwd=; database=menu";
         public Minitable()
