673672fec2e0332f2ed2e9634a2e63a0b54b61c9
Karl Melecio <karlmelecio04@gmail.com>
2019-11-07 16:52:18 +0800

    added comment

--- History.cs
+++ History.cs
@@ -20,7 +20,13 @@ namespace Cashier
     {
         //string myConnectionString = "server=192.168.15.8; uid=root; pwd=; database=menu";
         //string myConnectionString = "server=192.168.0.105; uid=roota; pwd=; database=menu";
+              //string myConnectionString = "server=192.168.15.8; uid=root; pwd=; database=menu";
+        //string myConnectionString = "server=192.168.0.105; uid=roota; pwd=; database=menu";
         
+        //string myConnectionString = "server=192.168.15.6; uid=root; pwd=; database=menu";
+        //string myConnectionString = "server=localhost; uid=root; pwd=; database=menu";
+
+
         //string myConnectionString = "server=192.168.15.6; uid=root; pwd=; database=menu";
         //string myConnectionString = "server=localhost; uid=root; pwd=; database=menu";
 
