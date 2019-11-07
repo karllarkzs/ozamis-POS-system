24f6490b55807126a5fbd12f32a5ea7079037efc
Karl Melecio <karlmelecio04@gmail.com>
2019-11-07 17:58:42 +0800

    added comment

--- Menu.cs
+++ Menu.cs
@@ -16,7 +16,13 @@ namespace Cashier
 {
     public partial class Menu : UserControl
     {
-      
+            //string myConnectionString = "server=192.168.15.8; uid=root; pwd=; database=menu";
+        //string myConnectionString = "server=192.168.0.105; uid=roota; pwd=; database=menu";
+        
+        //string myConnectionString = "server=192.168.15.6; uid=root; pwd=; database=menu";
+        //string myConnectionString = "server=localhost; uid=root; pwd=; database=menu";
+
+
         //string myConnectionString = "server=192.168.15.6; uid=root; pwd=; database=menu";
         //string myConnectionString = "server=localhost; uid=root; pwd=; database=menu";
 
