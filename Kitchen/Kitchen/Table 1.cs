a3b4c6ad6553f06889889ec90dfc03a3199b7523
Karl Melecio <karlmelecio04@gmail.com>
2019-11-21 18:09:46 +0800

    table

--- Kitchen/Kitchen/Table 1.cs
+++ Kitchen/Kitchen/Table 1.cs
@@ -22,6 +22,14 @@ namespace Kitchen
         
         //string myConnectionString = "server=192.168.15.6; uid=root; pwd=; database=menu";
         //string myConnectionString = "server=localhost; uid=root; pwd=; database=menu";
+ string myConnectionString = "server=localhost; uid=root; pwd=; database=menu";
+        public Form1()
+        {
+            InitializeComponent();
+            txtuser.Focus();
+
+        }
+
 
         string myConnectionString = "server=192.168.0.105; uid=roota; pwd=; database=menu";
         public Table_1()
